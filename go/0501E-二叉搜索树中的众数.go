765c6e3da56bfaeede9f8a2e6d78a50309832f71
Suki <1275506441@qq.com>
2021-11-11 21:14:23 +0800

    day++

---
+++ go/0501E-二叉搜索树中的众数.go
@@ -0,0 +1,39 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findMode(root *TreeNode) []int {
+	ans := make([]int, 0)
+	maxCount, cnt := 0, 0
+	var pre *TreeNode
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inOrder(node.Left)
+
+		if pre == nil {
+			cnt = 1
+		} else if pre.Val == node.Val {
+			cnt++
+		} else {
+			cnt = 1
+		}
+		pre = node
+
+		if cnt == maxCount {
+			ans = append(ans, node.Val)
+		} else if cnt > maxCount {
+			maxCount = cnt
+			ans = []int{node.Val}
+		}
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return ans
+}
---
+++ go/0530E-二叉搜索树的最小绝对差.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func getMinimumDifference(root *TreeNode) int {
+	var pre *TreeNode
+	ans := int(1e5)
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inOrder(node.Left)
+		if pre != nil {
+			if node.Val-pre.Val < ans {
+				ans = node.Val - pre.Val
+			}
+		}
+		pre = node
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return ans
+}
